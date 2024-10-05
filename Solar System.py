e747ff4e48e608708019da52764a300e8159c0f0
saashahwaiz8 <saashahwaiz8@gmail.com>
2024-10-05 13:24:13 +0500

    Update Solar System.py
    This is the code!

--- Solar System.py
+++ Solar System.py
@@ -5,32 +5,31 @@ app = Ursina()
 
 # Set the window title
 window.title = 'Solar System'
-window.fullscreen = False
 
 # Create the EditorCamera for editing
 camera = EditorCamera()
 
 # Create a Sun
-sun = Entity(model='sphere', scale=2, texture = 'sun.png')
+sun = Entity(model='sphere', scale=2, color=color.yellow)  # Replaced texture with color
 sun.collider = 'sphere'  # Enable collision for the sun
 
-# Define planets as a list of dictionaries
+# Define planets as a list of dictionaries with colors
 planets = [
-    {'name': 'Mercury', 'distance': 4, 'scale': 0.2, 'speed': 0.05, 'texture' : 'mercury.png'},
-    {'name': 'Venus', 'distance': 6, 'scale': 0.4, 'speed': 0.03, 'texture' : 'venus.png'},
-    {'name': 'Earth', 'distance': 8, 'scale': 0.5, 'speed': 0.02, 'texture' : 'earth.png'},
-    {'name': 'Mars', 'distance': 10, 'scale': 0.3, 'speed': 0.015, 'texture' : 'mars.png'},
-    {'name': 'Jupiter', 'distance': 12, 'scale': 0.7, 'speed': 0.01, 'texture' : 'jupiter.png'},
-    {'name': 'Saturn', 'distance': 14, 'scale': 0.6, 'speed': 0.008, 'texture' : 'saturn.png'},
-    {'name': 'Uranus', 'distance': 16, 'scale': 0.5, 'speed': 0.005, 'texture' : 'uranus.png'},
-    {'name': 'Neptune', 'distance': 18, 'scale': 0.5, 'speed': 0.004, 'texture' : 'neptune.png'},
+    {'name': 'Mercury', 'distance': 4, 'scale': 0.2, 'speed': 0.05, 'color': color.gray},
+    {'name': 'Venus', 'distance': 6, 'scale': 0.4, 'speed': 0.03, 'color': color.orange},
+    {'name': 'Earth', 'distance': 8, 'scale': 0.5, 'speed': 0.02, 'color': color.blue},
+    {'name': 'Mars', 'distance': 10, 'scale': 0.3, 'speed': 0.015, 'color': color.red},
+    {'name': 'Jupiter', 'distance': 12, 'scale': 0.7, 'speed': 0.01, 'color': color.brown},
+    {'name': 'Saturn', 'distance': 14, 'scale': 0.6, 'speed': 0.008, 'color': color.yellow},
+    {'name': 'Uranus', 'distance': 16, 'scale': 0.5, 'speed': 0.005, 'color': color.cyan},
+    {'name': 'Neptune', 'distance': 18, 'scale': 0.5, 'speed': 0.004, 'color': color.blue},
 ]
 
-# Create planets
+# Create planets with colors instead of textures
 for planet in planets:
     planet_entity = Entity(
         model='sphere',
-        texture=planet['texture'],
+        color=planet['color'],  # Replaced texture with color
         scale=planet['scale'],
         position=(planet['distance'], 0, 0)
     )
@@ -50,6 +49,6 @@ def update():
         )
 
 window.fullscreen = True
-Sky(texture = 'space.jpg')
+Sky(texture='space.jpg')
 
 app.run()
